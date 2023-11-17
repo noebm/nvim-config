20cb56a7094da81bcb66e6a6323b3bbac4328e3c
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-17 07:17:56 +0100

    setup completion sources

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,10 +11,23 @@ return
   -- "L3MON4D3/LuaSnip",
   },
   config = function()
-    require('cmp').setup {
-      -- sources = {
-      --   { name = 'nvim_lsp'}
-      -- }
+    local cmp = require("cmp")
+
+    cmp.setup {
+      sources = {
+        { name = "nvim_lsp"},
+        { name = "nvim_lua"},
+        { name = "buffer"},
+        { name = "path"},
+      }
     }
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        { name = "cmdline" },
+      })
+    })
   end
 }
