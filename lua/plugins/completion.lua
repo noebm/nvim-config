c92f140ab0da0b1c74265c678e3d541c220bf6f7
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-17 07:17:56 +0100

    execute formatter

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,23 +2,23 @@ return
 {
   "hrsh7th/nvim-cmp",
   dependencies = {
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-nvim-lua",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  -- "saadparwaiz1/cmp_luasnip",
-  -- "L3MON4D3/LuaSnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    -- "saadparwaiz1/cmp_luasnip",
+    -- "L3MON4D3/LuaSnip",
   },
   config = function()
     local cmp = require("cmp")
 
     cmp.setup {
       sources = {
-        { name = "nvim_lsp"},
-        { name = "nvim_lua"},
-        { name = "buffer"},
-        { name = "path"},
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "buffer" },
+        { name = "path" },
       }
     }
     cmp.setup.cmdline(":", {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,101 +1,101 @@
 return
 {
-"folke/which-key.nvim",
--- for nvim internal documentation
-{ "folke/neodev.nvim", config=true },
+  "folke/which-key.nvim",
+  -- for nvim internal documentation
+  { "folke/neodev.nvim",       config = true },
 
-{ "RRethy/vim-illuminate" },
-{ "windwp/nvim-autopairs", config=true },
+  { "RRethy/vim-illuminate" },
+  { "windwp/nvim-autopairs",   config = true },
 
--- lspconfig / mason setup
-{ "williamboman/mason.nvim", config=true },
-{ "neovim/nvim-lspconfig",
-  dependencies =
+  -- lspconfig / mason setup
+  { "williamboman/mason.nvim", config = true },
   {
-  { "folke/neoconf.nvim", cmd = "Neoconf", config=true },
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim"
+    "neovim/nvim-lspconfig",
+    dependencies =
+    {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim"
+    },
   },
-  },
-  { "williamboman/mason-lspconfig.nvim",
-  dependencies = { "williamboman/mason.nvim" },
-  config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "lua_ls", "pyright",
-      },
-      automatic_installation = true,
-    })
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "lua_ls", "pyright",
+        },
+        automatic_installation = true,
+      })
 
-    mason_lspconfig.setup_handlers({
-      function(server_name)
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          local opts = {}
 
-        local opts = {}
-
-        if server_name == "lua_ls" then
-          require("neodev").setup({})
-          opts.settings = {
-            Lua = {
-              completion = { callSnippet = "Replace" },
-              workspace = { checkThirdParty = false },
-              diagnostics = {
-                -- globals = 'vim',
-                disable = { 'missing-fields '}
+          if server_name == "lua_ls" then
+            require("neodev").setup({})
+            opts.settings = {
+              Lua = {
+                completion = { callSnippet = "Replace" },
+                workspace = { checkThirdParty = false },
+                diagnostics = {
+                  -- globals = 'vim',
+                  disable = { 'missing-fields ' }
+                }
               }
             }
-          }
+          end
 
+          opts.capabilities = require("cmp_nvim_lsp").default_capabilities()
+          opts.on_attach = function(client, buffer)
+            require("illuminate").on_attach(client)
+            require("config/lsp/keymaps").on_attach(client, buffer)
+          end
+
+          require("lspconfig")[server_name].setup(opts)
         end
-
-        opts.capabilities = require("cmp_nvim_lsp").default_capabilities()
-        opts.on_attach = function(client, buffer)
-          require("illuminate").on_attach(client)
-          require("config/lsp/keymaps").on_attach(client, buffer)
-        end
-
-        require("lspconfig")[server_name].setup(opts)
-      end
-    })
-  end
-},
-{ "jose-elias-alvarez/null-ls.nvim", config=true },
-
-{
-  "jay-babu/mason-null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "williamboman/mason.nvim",
-    "jose-elias-alvarez/null-ls.nvim",
+      })
+    end
   },
-  config = function()
-    -- require("your.null-ls.config") -- require your null-ls config here (example below)
-    require("mason-null-ls").setup({
-      ensure_installed = { "jq" }
-    })
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  { "jose-elias-alvarez/null-ls.nvim", config = true },
 
-    local null_ls = require("null-ls")
-    local sources = {
-      null_ls.builtins.formatting.stylua,
-    }
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+      -- require("your.null-ls.config") -- require your null-ls config here (example below)
+      require("mason-null-ls").setup({
+        ensure_installed = { "jq" }
+      })
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-            end,
-          })
-        end
-      end,
-      sources = sources,
-    })
-  end,
-}
+      local null_ls = require("null-ls")
+      local sources = {
+        null_ls.builtins.formatting.stylua,
+      }
+
+      null_ls.setup({
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format()
+              end,
+            })
+          end
+        end,
+        sources = sources,
+      })
+    end,
+  }
 
 }
