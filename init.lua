6d09a7a6201b725563b5f6ab62bc51a39735ca15
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-19 21:10:31 +0100

    add colorschemes and move selection to config/colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config/options")
 require("config/lazy")
 require("config/autocommands")
+require("config/colorscheme")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,4 @@
-return
-{
+return {
   "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd.colorscheme "tokyonight"
-  end
+  "EdenEast/nightfox.nvim",
 }
