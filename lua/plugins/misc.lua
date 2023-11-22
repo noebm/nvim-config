9c143124f9ea4facaf77a4a313d4d20a87113e64
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-23 00:37:41 +0100

    explaination for better escape / indent-blankline

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,9 +1,11 @@
 return {
+  -- show indentation levels
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {}
   },
+  -- press jj / jk instead of ESCAPE
   {
     "max397574/better-escape.nvim",
     config = true
