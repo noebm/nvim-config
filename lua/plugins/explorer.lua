d309dacc2fc8c492a915dd4778adee64b3d52518
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-23 23:42:31 +0100

    set ".." to backspace

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,7 +1,11 @@
 return {
   "stevearc/oil.nvim",
+  opts = {
+    keymaps = {
+      ["<BS>"] = "actions.parent"
+    }
+  },
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
-  config = true
 }
