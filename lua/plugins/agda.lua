83ab0b028a6ebf6ba4553324666da4497a78369d
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-25 02:27:26 +0100

    add agda plugin

---
+++ lua/plugins/agda.lua
@@ -0,0 +1,13 @@
+return {
+  "ashinkarov/nvim-agda",
+  ft = {
+    "lagda", "agda"
+  },
+  -- opts = {},
+  config = true,
+  --  function(plugin, opts)
+  --  local agda = require("agda")
+  --  agda.setup(opts)
+  --  -- require("agda-input")
+  --end
+}
