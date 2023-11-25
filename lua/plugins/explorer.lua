8faa3dd8565f1d5a865eccc74f9549066dba5369
Moritz Nöbauer <moritz.noebauer@gmail.com>
2023-11-25 13:07:36 +0100

    add project tree (nvim-tree) with keybind

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,11 +1,26 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {
-    keymaps = {
-      ["<BS>"] = "actions.parent"
-    }
+  -- filesystem buffers
+  -- allows editing directory like its a text file
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      keymaps = {
+        ["<BS>"] = "actions.parent"
+      }
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
   },
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
+  -- project tree
+  {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      { "<leader>p", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle project tree" } }
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = true
+  }
 }
